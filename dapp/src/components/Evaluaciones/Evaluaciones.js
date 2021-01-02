e373eae32e5e9b9f43376acf971ec324d4c45efb
Daveni <veranieto.dani@gmail.com>
2021-01-02 21:02:27 +0100

    New evaluations form added. TODO: Fix date selection

--- dapp/src/components/Evaluaciones/Evaluaciones.js
+++ dapp/src/components/Evaluaciones/Evaluaciones.js
@@ -2,6 +2,8 @@ import {newContextComponents} from "@drizzle/react-components";
 
 import EvaluacionesHead from "./EvaluacionesHead";
 import EvaluacionesBody from "./EvaluacionesBody";
+import NuevaEvaluacion from "./NuevaEvaluacion";
+
 
 import EvaluacionesSinComponentes from "./EvaluacionesSinComponentes";
 
@@ -9,7 +11,12 @@ const {ContractData} = newContextComponents;
 
 const Evaluaciones = (props) => (
     <section className="AppEvaluaciones">
+        
+        <NuevaEvaluacion drizzle={props.drizzle}
+                   drizzleState={props.drizzleState} />
         <h2>Evaluaciones</h2>
+
+        
 
         <ContractData
             drizzle={props.drizzle}
---
+++ dapp/src/components/Evaluaciones/NuevaEvaluacion.js
@@ -0,0 +1,22 @@
+import {newContextComponents} from "@drizzle/react-components";
+const {ContractData, ContractForm} = newContextComponents;
+
+const NuevaEvaluacion = ({drizzle, drizzleState}) => <article className="AppMisDatos">
+    <h3>Nueva Evaluación</h3>
+    <ContractData drizzle={drizzle} drizzleState={drizzleState}
+                  contract={"Asignatura"} method={"profesor"} methodArgs={[]}
+                  render={addr => {
+                      if (addr !== drizzleState.accounts[0]) {
+                          return <p>"Para crear evaluaciones debes ser el profesor"</p>
+                      }
+                      return <ContractForm drizzle={drizzle} drizzleState={drizzleState}
+                                           contract="Asignatura" method="creaEvaluacion"
+                                           labels={["Nombre de la evaluacion (Examen, Entrega,etc)",
+                                            "Fecha (segundos desde el 1/1/1970)",
+                                             "Puntos (x10) sobre la nota final"]}
+                      />
+                  }}
+    />
+</article>
+
+export default NuevaEvaluacion;
